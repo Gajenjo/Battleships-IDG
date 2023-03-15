5d25cdc04cef6e654e2742f9b03e9d1f53d2e068
Dustin <dustinhwhite@gmail.com>
2023-03-15 14:37:35 +0100

    Dustin boat variables

---
+++ constants.py
@@ -0,0 +1,11 @@
+# Write all variables
+size = 10
+boat1=4
+boat2=3
+boat3=2
+boat4=1
+boat="\u26F4"
+miss = "x"
+#find emoji of shot missing in water 
+# boat_damaged = find emoji: 
+boat_damaged = "\u1F525" 
