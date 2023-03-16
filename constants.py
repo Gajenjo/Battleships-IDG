1aa072a4fda57963ea9b6a175e6124dd4a2086c8
Dustin <dustinhwhite@gmail.com>
2023-03-16 17:12:30 +0100

    new constants

--- constants.py
+++ constants.py
@@ -1,11 +1,15 @@
 # Write all variables
+# “U+1F600” will become “U0001F600” and prefix the unicode with “\” and print it.
+
 size = 10
-boat1=4
-boat2=3
-boat3=2
-boat4=1
-boat="\u26F4"
-miss = "x"
-#find emoji of shot missing in water 
-# boat_damaged = find emoji: 
-boat_damaged = "\u1F525" 
+boat1 = 4
+boat2 = 3
+boat3 = 2
+boat4 = 1
+boat ="\u26F4"
+miss = "\U0001F4A7"
+boat_damaged = "\U0001F525"
+
+print (boat_damaged)
+print (boat)
+print(miss)
