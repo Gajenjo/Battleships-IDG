ba756964cf340abb3ca0ec8b973d109cda954626
Dustin <dustinhwhite@gmail.com>
2023-03-19 13:39:34 +0100

    actualizacion constants

--- constants.py
+++ constants.py
@@ -1,6 +1,3 @@
-# Write all variables
-# “U+1F600” will become “U0001F600” and prefix the unicode with “\” and print it.
-
 SIZE = 10
 BOAT1 = 4
 BOAT2 = 3
@@ -9,8 +6,3 @@ BOAT4 = 1
 BOAT ="\u26F4"
 MISS = "\U0001F4A7"
 BOAT_DAMAGED = "\U0001F525"
-
-print (BOAT)
-print (BOAT_DAMAGED)
-print(MISS)
-print()
